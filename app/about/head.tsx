1c19504688b25a17c1f20d657d307e56df000b6a
dcgoodwin2112 <dan.goodwin@civicactions.com>
2022-12-04 21:33:05 -0500

    Add head component for about page

---
+++ app/about/head.tsx
@@ -0,0 +1,8 @@
+export default function AboutHead() {
+  return (
+    <>
+      <title>I am the Dan</title>
+      <meta name="description" content="Dan's random bio generator"></meta>
+    </>
+  );
+}
