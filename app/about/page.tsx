bbfd5d82d31334f6505e22bc25e71ff5f63032ff
dcgoodwin2112 <dan.goodwin@civicactions.com>
2022-12-06 12:21:41 -0500

    update page template

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,10 +3,8 @@ import styles from "./page.module.css";
 
 export default function AboutPage() {
   return (
-    <main>
       <div className={styles.container}>
         <About />
       </div>
-    </main>
   );
 }
