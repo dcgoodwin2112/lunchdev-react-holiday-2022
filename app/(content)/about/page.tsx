8353fa07c4df5f3f9817991de73351c710240f77
dcgoodwin2112 <dan.goodwin@civicactions.com>
2022-12-12 23:28:08 -0500

    env cleanup

--- app/(content)/about/page.tsx
+++ app/(content)/about/page.tsx
@@ -2,13 +2,13 @@ import About from "./about";
 import styles from "./page.module.css";
 
 export default async function AboutPage() {
-  let req = await fetch("http://localhost:3000/api/animals");
+  let req = await fetch(`${process.env.ROOT_URL}/api/animals`);
   // let req = await fetch("https://httpstat.us/500");
   let animal: { name: string } = await req.json();
   return (
-      <div className={styles.container}>
-        <About />
-        <p>PS: It has been rumored that Dan was raised by: {animal.name}</p>
-      </div>
+    <div className={styles.container}>
+      <About />
+      <p>PS: It has been rumored that Dan was raised by: {animal.name}</p>
+    </div>
   );
 }
